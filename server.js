ad7ab0e0448fa5764c6ccf64a37e8d2389e1eab5
Nikita Shaban <shaban.nikitka@gmail.com>
2021-06-14 15:36:15 +0300

    add express

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 app.get('/', (request, response) => {
 response.send('Hello from Express!')
 })
+
 app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
