7a30cc24deca36e1682f38e70c9bdbdefa1ac302
Nikita Shaban <shaban.nikitka@gmail.com>
2021-06-14 19:05:16 +0300

    add get and post

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+
+const app = express();
+
+const jsonParser = express.json();
+
+const todoList = [{value: "Сходить потусить", isDone: false, id: 0}]
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE, OPTIONS");
+    next();
+});
+
+app.post("/all", jsonParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+
+    todoList.push(request.body)
+
+    response.json(todoList);
+});
 
 app.get('/', (request, response) => {
-response.send('Hello from Express!')
+    response.send(todoList);
 })
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`server is listening on ${port}`)
-})
+app.listen(3000);
